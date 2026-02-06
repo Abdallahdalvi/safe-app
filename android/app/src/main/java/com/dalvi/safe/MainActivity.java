48f9fe0baa4df247c3d8f7832b2112264de83813
Your Name <your-email@example.com>
2026-02-06 12:24:30 +0530

    Fix build error: add missing PendingIntent import

--- android/app/src/main/java/com/dalvi/safe/MainActivity.java
+++ android/app/src/main/java/com/dalvi/safe/MainActivity.java
@@ -3,6 +3,7 @@ package com.dalvi.safe;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ViewGroup;
